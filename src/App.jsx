58dde1d97bee6d99c2e440522fd2b29e6bf096b2
reza <reza.torkanlu.81@gmail.com>
2024-09-02 01:34:04 +0330

    add header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {BrowserRouter , Routes, Route} from 'react-router-dom';
+import Header from './components/Header';
+function App() {
+  return (
+        <BrowserRouter>
+          <Header />
+          
+          
+        </BrowserRouter>  
+  );
+}
+
+export default App;
