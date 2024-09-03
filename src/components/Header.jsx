86fe31fc447ab9ea911c1d5c15cea716e625657e
RezaTorkanlu <149518241+RezaTorkanlu@users.noreply.github.com>
2024-09-03 13:56:33 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
                     {showNav ?
                          <ul className='flex justify-between w-3/5 max-md:fixed max-md:top-24 max-md:border max-md:rounded max-md:shadow-lg max-md:h-5/6 max-md:flex-col max-md:justify-around max-md:w-11/12 bg-white'>
                               <li><Link className='mx-3  max-md:p-5  max-md:border-b max-md:block  hover:text-green-600' onClick={toggleNav} to="/">Home</Link></li>
-                              <li><Link className='mx-3  max-md:p-5  max-md:border-b max-md:block  hover:text-green-600' onClick={toggleNav} to="/products">Products</Link></li>
+                              <li><Link className='mx-3  max-md:p-5  max-md:border-b max-md:block  hover:text-green-600' onClick={toggleNav} to="/">Products</Link></li>
                               <li><Link className='mx-3  max-md:p-5  max-md:border-b max-md:block  hover:text-green-600' onClick={toggleNav} to="/">About us</Link></li>
                               <li><Link className='mx-3  max-md:p-5  max-md:border-b max-md:block  hover:text-green-600' onClick={toggleNav} to="/">contact</Link></li>
                               <li><Link className='mx-3  max-md:p-5  max-md:border-b max-md:block  hover:text-green-600' onClick={toggleNav} to="/"> login</Link></li>
